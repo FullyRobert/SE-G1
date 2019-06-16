cb748cdf1d036976d1ff901ca479d9e9818361b3
柴子炜 <3170105763@zju.edu.cn>
2019-06-16 15:42:27 +0800

    15:42

--- models/accountModel.js
+++ models/accountModel.js
@@ -418,5 +418,42 @@ module.exports = {
     }catch(err){
         callback(err, undefined);
     }
+  },
+  /*
+ * @功能: 查询余额
+ * @作者: 刘长硕
+ */
+  balance: async function (req, callback) {
+    try {
+      const conn = await pool.getConnection();
+      let id = req.session.token.uid;
+      let balance = "select balance,username from user where id = '" + id + "'";
+      ret = await conn.query(balance);
+      callback(undefined, ret[0]);
+      conn.release();
+    } catch (err) {
+      callback(err, undefined);
+    }
+  },
+
+  updatebalance: async function (req, callback) {
+    try {
+      const conn = await pool.getConnection();
+      let sid = req.session.token.uid;
+      let balance = req.body.balance;
+      let charge = req.body.charge;
+      let username = req.body.username;
+      let newbalance = parseInt(balance) + parseInt(charge);
+      newbalance = parseInt(newbalance);
+      ret = 0;
+      let a = "update user set balance = " + newbalance + " where id ='" + sid + "'";
+      ret = await conn.query(a);
+      callback(undefined, ret[0]);
+
+      conn.release();
+      callback(undefined, ret[0]);
+    } catch (err) {
+      callback(err, undefined);
+    }
   }
 };
